5928304f58502ab524e80d8eeec611a92a3d98ed
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-04-18 22:25:52 -0400

    Add basic login page layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  public loginGroup: FormGroup;
+
+  ngOnInit() {
+    this.loginGroup = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  submitLogin() {
+    console.log('Login submitted');
+  }
 
 }
