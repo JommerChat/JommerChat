a97203f60596b7d2acfe35fc96b6a7e35aed694e
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-07-12 21:00:07 -0400

    Add auth service for custom okta login and logout

---
+++ src/app/shared/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {OktaAuth} from '@okta/okta-auth-js';
+import {environment} from '../../../environments/environment';
+import {BehaviorSubject} from 'rxjs';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private authClient = new OktaAuth({
+    issuer: environment.issuer,
+    clientId: environment.clientId
+  });
+
+  public isAuthenticated = new BehaviorSubject<boolean>(false);
+
+  constructor(private router: Router) { }
+
+  async checkAuthenticated(): Promise<boolean> {
+    const authenticated = await this.authClient.session.exists();
+    this.isAuthenticated.next(authenticated);
+    return authenticated;
+  }
+
+  async login(username: string, password: string): Promise<void> {
+    const transaction = await this.authClient.signIn({username, password});
+
+    if (transaction.status !== 'SUCCESS') {
+      throw Error(`Authentication error occurred with status: ${transaction.status}`);
+    }
+    this.isAuthenticated.next(true);
+    this.authClient.session.setCookieAndRedirect(transaction.sessionToken);
+  }
+
+  async logout(redirect: string): Promise<void> {
+    try {
+      await this.authClient.signOut();
+      this.isAuthenticated.next(false);
+      await this.router.navigate([redirect]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
