fcebb388880078d74c11183148b87970070b7cdb
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-08-08 15:44:19 -0400

    Add MemberEntity and repo to persist members to database

--- BackEnd/auth/src/main/java/com/parlantos/auth/repo/MemberRepository.java
+++ BackEnd/auth/src/main/java/com/parlantos/auth/repo/MemberRepository.java
@@ -1,2 +1,17 @@
-package com.parlantos.auth.repo;public class MemberRepository {
+package com.parlantos.auth.repo;
+
+import com.parlantos.auth.beans.MemberEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public interface MemberRepository extends CrudRepository<MemberEntity, BigInteger> {
+
+  public List<MemberEntity> findByEmail(String email);
+
+  @Query("SELECT m from MemberEntity m where m.display_name = :displayName ORDER BY m.display_name DESC")
+  public List<MemberEntity> findLastTagByDisplayName(@Param("displayName") String displayName);
 }
