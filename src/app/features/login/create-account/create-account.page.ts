094a000abab88cbd2691931b9a2393565a41f487
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-09-12 18:13:32 -0400

    Finish wiring up registration page to okta

--- src/app/features/login/create-account/create-account.page.ts
+++ src/app/features/login/create-account/create-account.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CreateAccountService} from './create-account.service';
+import {Subscription} from 'rxjs';
+import {AuthService} from '../../../shared/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
@@ -8,22 +12,45 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class CreateAccountPage implements OnInit {
 
+  public registerObservable$: Subscription;
   public createAccountGroup: FormGroup;
   public hidePassword = true;
+  public meetsPasswordComplexity = true;
 
-  constructor() { }
+  constructor(private createAccountService: CreateAccountService, private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.createAccountGroup = new FormGroup({
       email: new FormControl('name@email.com', [Validators.required, Validators.email]),
       username: new FormControl(null, [Validators.required]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
       repeatPassword: new FormControl(null, [Validators.required])
     });
+    if (await this.authService.checkAuthenticated()) {
+      await this.router.navigateByUrl('navbar/chat');
+    }
   }
 
   changePasswordView() {
     this.hidePassword = !this.hidePassword;
+  }
+
+  async registerUser() {
+    this.registerObservable$ = this.createAccountService.registerUser(this.createAccountGroup.get('email').value,
+      this.createAccountGroup.get('username').value, this.createAccountGroup.get('password').value).subscribe((result) => {
+        this.meetsPasswordComplexity = true;
+        console.log('Logging result from request: ' + JSON.stringify(result));
+        this.submitLogin();
+    }, (error) => {
+        console.log(`Error object value: {}`, JSON.stringify(error));
+        if (error.error.includes('Okta E0000001')) {
+          this.meetsPasswordComplexity = false;
+        }
+    });
+  }
+
+  async submitLogin() {
+    await this.authService.login(this.createAccountGroup.get('email').value, this.createAccountGroup.get('password').value);
   }
 
 }
