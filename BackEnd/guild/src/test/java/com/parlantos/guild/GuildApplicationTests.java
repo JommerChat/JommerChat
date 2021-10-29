4397e5b589bfd1598489847dd23e91b5e98a66e4
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-10-28 22:50:51 -0400

    Add remaining guild service project files

---
+++ BackEnd/guild/src/test/java/com/parlantos/guild/GuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parlantos.guild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
