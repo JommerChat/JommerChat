15bf45d8e61fb093221f4cfb1876eb74246d258d
Johnathan Bressette <bressetteprogramming@gmail.com>
2021-11-27 14:51:25 -0500

    Add background colors and fixed sizing of guild columns

--- src/app/features/servers/guild/channels/channels.component.ts
+++ src/app/features/servers/guild/channels/channels.component.ts
@@ -3,9 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-channels',
   templateUrl: './channels.component.html',
-  styleUrls: ['./channels.component.scss'],
+  styleUrls: ['./channels.component.scss']
 })
 export class ChannelsComponent implements OnInit {
+
+  guildName = 'Test Name';
+
+  textChannels = [
+    {
+    id: 1234,
+    created_at: new Date(),
+    guild_id: 1234,
+    title: 'general',
+    description: 'General chat',
+    categoryId: '',
+    position: 0
+    },
+    {
+    id: 1234,
+    created_at: new Date(),
+    guild_id: 1234,
+    title: 'memes',
+    description: 'Meme channel',
+    categoryId: '',
+    position: 1
+}];
 
   constructor() { }
 
