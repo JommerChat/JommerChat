c67b171480442b209304731c65c73dbac58a4853
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-01-13 17:32:13 -0500

    Add local express server changes for local testing

--- LocalServer/GuildOrchestrator.js
+++ LocalServer/GuildOrchestrator.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const app = express();
 const port = 8080;
 const baseUrl = '/guild';
+const cors = require('cors');
+
+app.use(cors());
 
 app.get(`${baseUrl}/initialInfo`, async (req, resp) => {
   const initialInfo = JSON.parse(fs.readFileSync('/home/bressette/git/parlantos/LocalServer/initialInfo.json'));
