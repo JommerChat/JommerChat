8479512c88a21b2965145c8436943f95eb74b346
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-01-19 20:41:20 -0500

    Add initial version of members ui

--- src/app/features/servers/guild/members/members.component.ts
+++ src/app/features/servers/guild/members/members.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersComponent implements OnInit {
 
-  constructor() { }
+  members: Array<{ icon: string, username: string; }> = [];
+
+  constructor() {
+    let icon = 'https://cdn.discordapp.com/avatars/304475016936816640/a_7cde5bfd9549b15e98d17eb24d758593.webp?size=96';
+    let username = 'HaZe (Jom)';
+    this.members.push({icon, username});
+    icon = 'https://cdn.discordapp.com/avatars/326546102537158666/7f2ade2fd4f415e307aa277e0e2b137b.webp?size=96';
+    username = 'Annual';
+    this.members.push({icon, username});
+  }
 
   ngOnInit() {}
 
