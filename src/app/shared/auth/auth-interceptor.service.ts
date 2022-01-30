52abf2603c5e503f849e48fb04179d6021c5e92c
Johnathan Bressette <bressetteprogramming@gmail.com>
2022-01-30 17:44:46 -0500

    Add auth-interceptor to add the okta access token to all requests

---
+++ src/app/shared/auth/auth-interceptor.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${this.authService.getAccessToken()}`
+      },
+    });
+
+    return next.handle(req);
+  }
+}
